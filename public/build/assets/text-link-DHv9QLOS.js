ccabe690c098c61cdfff617db01778f38935fe54
Pblo16 <pablo.hv.2002@gmail.com>
2025-09-25 16:51:33 -0600

    Build

--- public/build/assets/text-link-DHv9QLOS.js
+++
@@ -1,1 +0,0 @@
-import{j as t,L as n}from"./app-DziIGFx9.js";import{c as a}from"./createLucideIcon-BOtGqX0d.js";function u({className:r="",children:e,...o}){return t.jsx(n,{className:a("text-foreground underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out hover:decoration-current! dark:decoration-neutral-500",r),...o,children:e})}export{u as T};
