a90d3624a7d21ba4b80033b8d40ada7884a6c201
Pblo16 <pablo.hv.2002@gmail.com>
2025-09-25 16:59:22 -0600

    feat: add UserForm component for user creation and editing with validation

---
+++ public/build/assets/input-error-DFt-6bIU.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-0NG33R1C.js";import{c as o}from"./button-CUjZfIwL.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
