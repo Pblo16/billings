98f08ab85fe6c4f81ad3975a8d607a38a89bfb18
Pblo16 <pablo.hv.2002@gmail.com>
2025-09-30 13:10:16 -0600

    feat: update Users breadcrumb link to use dynamic route

--- resources/js/pages/users/Index.tsx
+++ resources/js/pages/users/Index.tsx
@@ -1,13 +1,14 @@
 import { DataTable } from '@/components/data-table'
 import AppLayout from '@/layouts/app-layout'
 import { columns } from '@/pages/users/columns'
+import { users } from '@/routes'
 import { create } from '@/routes/users'
 import { type BreadcrumbItem, type User } from '@/types'
 
 const breadcrumbs: BreadcrumbItem[] = [
   {
     title: 'Users',
-    href: '/users',
+    href: users().url,
   },
 ]
 
